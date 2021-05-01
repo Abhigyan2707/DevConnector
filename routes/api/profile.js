acf7ee431cb5b8768f2a0fa1bf44ac8c44d35cfa
abhigyan bhardwaj <theabhigyan00@gmail.com>
2021-05-01 10:41:58 +0530

    commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
-//@route    GET api/profile
-//@desc     Test Route
-//@access   Public
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+//@route    GET api/profile/me
+//@desc     Test Route
+//@access   Private
+
+router.get('/me', auth, async (req, res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.user.id}).populate('user', ['name', 'avatar']);
+        if(!profile){
+            return res.status(400).json({ msg: 'There is no profile for this user'});
+        }
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+//@route    POST api/profile
+//@desc     Create or update user profile
+//@access   Private
+
 
 
 module.exports = router;
